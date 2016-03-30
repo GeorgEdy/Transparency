8b24b2279f523fefa168044319ead27e50e693a6
Roxana Babatie <roxana.babatie@gmail.com>
2016-03-30 21:21:41 +0300

    open modal on button click

--- js/build.js
+++ js/build.js
@@ -25,6 +25,28 @@ app.config(function ($locationProvider, $routeProvider) {
             redirectTo: '/'
         });
 });
+
+app.directive('modalDialog', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            show: '='
+        },
+        replace: true, // Replace with the template below
+        transclude: true, // we want to insert custom content inside the directive
+        link: function(scope, element, attrs) {
+            scope.dialogStyle = {};
+            if (attrs.width)
+                scope.dialogStyle.width = attrs.width;
+            if (attrs.height)
+                scope.dialogStyle.height = attrs.height;
+            scope.hideModal = function() {
+                scope.show = false;
+            };
+        },
+        template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
+    };
+});
 ;app.controller("ComparisonCtrl",function($scope){
 
 });
@@ -32,5 +54,13 @@ app.config(function ($locationProvider, $routeProvider) {
 
 });
 ;app.controller("HomeCtrl",function($scope){
-
+    $scope.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+    ];
+    $scope.openModal = function () {
+        $scope.modalShown = false;
+        $scope.modalShown = !$scope.modalShown;
+    };
 });;
