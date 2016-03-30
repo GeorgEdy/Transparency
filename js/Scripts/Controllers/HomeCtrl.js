dfedba1b8fac56747d8ca5ce06b47c7e1cf87cb3
Roxana Babatie <roxana.babatie@gmail.com>
2016-03-30 22:36:40 +0300

    removed awesomeThings

--- js/Scripts/Controllers/HomeCtrl.js
+++ js/Scripts/Controllers/HomeCtrl.js
@@ -1,9 +1,4 @@
 app.controller("HomeCtrl",function($scope){
-    $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
-    ];
     $scope.openModal = function () {
         $scope.modalShown = false;
         $scope.modalShown = !$scope.modalShown;
--- js/build.js
+++ js/build.js
@@ -54,13 +54,9 @@ app.directive('modalDialog', function() {
 
 });
 ;app.controller("HomeCtrl",function($scope){
-    $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
-    ];
     $scope.openModal = function () {
         $scope.modalShown = false;
         $scope.modalShown = !$scope.modalShown;
     };
+
 });;
