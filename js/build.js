ed9944b2aae3c3ae51908a10213ad00bc993c50c
Roxana Babatie <roxana.babatie@gmail.com>
2016-03-30 20:53:46 +0300

    added bootstrap.js

---
+++ js/build.js
@@ -0,0 +1,36 @@
+var app = angular.module("transparencyApp", ['ngRoute']);
+
+app.config(function ($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    $routeProvider
+        .when('/', {
+            templateUrl: 'Views/home.html',
+            controller: 'HomeCtrl',
+            controllerAs: 'home'
+        })
+        .when('/comparison', {
+            templateUrl: 'Views/comparison.html',
+            controller: 'ComparisonCtrl',
+            controllerAs: 'comparison'
+        })
+        .when('/filter', {
+            templateUrl: 'Views/filter.html',
+            controller: 'FilterCtrl',
+            controllerAs: 'filter'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+;app.controller("ComparisonCtrl",function($scope){
+
+});
+;app.controller("FilterCtrl",function($scope){
+
+});
+;app.controller("HomeCtrl",function($scope){
+
+});;
