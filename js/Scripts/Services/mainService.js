135c2123324d9ba9b8edca1f5d9067a2e6b0e2ad
Ionut Firatoiu <firatoiu.ionut@gmail.com>
2016-04-18 18:01:44 +0300

    First try! Undefined result

---
+++ js/Scripts/Services/mainService.js
@@ -0,0 +1,23 @@
+app.factory('DataStore', function ($http, $q) {
+    return (function () {
+        var URL = 'https://intense-sierra-23176.herokuapp.com/search';
+
+        var getAll = function () {
+            return $q(function (resolve, reject) {
+                $http({url: 'https://intense-sierra-23176.herokuapp.com/search'}).then(function (xhr) {
+                        if (xhr.status == 200) {
+                            resolve(xhr.data);
+                        } else {
+                            reject();
+                        }
+                    },
+                    reject
+                );
+            })
+        };
+
+        return {
+            getAll: getAll
+        };
+    });
+});
