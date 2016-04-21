ee825798eaf1d866c66d9ff16766b4b7e082e1db
GeorgEdy <eduard.dinu94@yahoo.ro>
2016-04-21 15:25:55 +0300

    Display total income and expense by county on hover.

--- js/build.js
+++ js/build.js
@@ -171,12 +171,35 @@ function AutocompleteCtrl ($timeout, $q, $log) {
         console.log("data ",$scope.data);
     })  
     }
-});;app.controller("HomeCtrl",function($scope){
+});;app.controller("HomeCtrl",function($scope, DataStore){
     $scope.openModal = function () {
         $scope.modalShown = false;
         $scope.modalShown = !$scope.modalShown;
     };
+    $scope.date =[];
+    $scope.showData = function () {
+        var incomeSum = 0;
+        var outcomeSum = 0;
+        DataStore.getAll().then(function (items) {
+            $scope.date = items;
 
+            items.forEach(function (index) {
+                if( index.judet ==='Bucuresti') {
+                    incomeSum += Math.round(index.venit);
+                    outcomeSum += Math.round(index.cheltuieli);
+                }
+            });
+            document.getElementById('income-hover').innerHTML = 'Total venituri: ' + incomeSum +'  mii lei';
+            document.getElementById('outcome-hover').innerHTML = 'Total cheltuieli: ' + outcomeSum +' mii lei';
+            return [incomeSum, outcomeSum];
+        });
+
+    };
+    $scope.hideData = function () {
+        document.getElementById('income-hover').innerHTML = '';
+        document.getElementById('outcome-hover').innerHTML = '';
+
+    }
 });;app.factory('DataStore', function ($http, $q) {
     return (function () {
         var URL = 'https://intense-sierra-23176.herokuapp.com/search';
