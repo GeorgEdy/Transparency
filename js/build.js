eba427f4f5c235128048e0a85b0a810b6d743781
Ionut Firatoiu <firatoiu.ionut@gmail.com>
2016-04-21 14:28:05 +0300

    returned data for chart is wrong

--- js/build.js
+++ js/build.js
@@ -159,18 +159,17 @@ function AutocompleteCtrl ($timeout, $q, $log) {
         $scope.modalShown = false;
         $scope.modalShown = !$scope.modalShown;
 
-        var venit = 0;
-        var cheltuieli = 0;
-        var chartData = [venit, cheltuieli];
+        var venit;
+        var cheltuieli;
 
         angular.forEach($scope.data, function (item) {
-            venit = item.venit;
-            cheltuieli = item.cheltuieli;
-            /*var tr = document.getElementsByTagName('tr');*/
-            var currentRow = $event.target;
+            var currentRow = $event.target.parentNode;
             var dataId = currentRow.getAttribute('data-id');
-            console.log(dataId);
+            venit = $scope.data[dataId-1].venit;
+            cheltuieli = $scope.data[dataId-1].cheltuieli;
         });
+
+        var chartData = [venit, cheltuieli];
 
         return chartData;
     };
@@ -178,7 +177,6 @@ function AutocompleteCtrl ($timeout, $q, $log) {
     $scope.data = [];
     DataStore.getAll().then(function (items) {
         $scope.data = items;
-        console.log("all: ", $scope.data);
     });
 
     $scope.search = function () {
@@ -186,26 +184,10 @@ function AutocompleteCtrl ($timeout, $q, $log) {
         $scope.institutie = document.getElementById('institutie').value;
         DataStore.getSearch($scope.arie, $scope.institutie).then(function (items) {
             $scope.data = items;
-            console.log("data ", $scope.data);
         })
     };
 
-    $scope.chartData = $scope.openModal;
-    /*function(){
-     /!*var venit = 0;
-     var cheltuieli = 0;
-     var chartData = [venit,cheltuieli];
-
-     angular.forEach($scope.data, function(item){
-     console.log(item);
-     venit = item.venit;
-     cheltuieli = item.cheltuieli;
-     console.log(venit);
-     console.log(cheltuieli);
-     });
-
-     return chartData;*!/
-     };*/
+    /*$scope.chartData = $scope.openModal;*/
 
     $scope.chartLabels = ['Venituri', 'Cheltuieli'];
 });;app.controller("HomeCtrl",function($scope){
